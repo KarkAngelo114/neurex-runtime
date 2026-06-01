e33edb1c82b7d54b84762d0cd39b37a50e993575
Kark Angelo <karkangelovaronapada@gmail.com>
2026-06-01 16:17:26 +0800

    ....

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ module.exports = [
             name: 'NeurexRuntime',
             exports: 'named'
         },
-        plugins: [resolve(), commonjs(), terser()]
+        plugins: [resolve(), commonjs()]
     },
     {
         input: 'index.js',
